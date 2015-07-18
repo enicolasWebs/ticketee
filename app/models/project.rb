db6f835ba19e28d50f6abb66feb72bb4ca0786b8
Ernald Nicolas <enicolas@vistaprint.com>
2015-07-18 01:22:39 -0400

    Cull when project gets destroyed

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ActiveRecord::Base
     validates :name, presence: true
     validates :description, presence: true
 
-    has_many :tickets
+    has_many :tickets, dependent: :delete_all
 end
