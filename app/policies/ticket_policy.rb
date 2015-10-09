5cdd6a2a198f5d32b07db4ff89b5ab79ae21c105
Ernald Nicolas <enicolas@vistaprint.com>
2015-10-09 00:39:08 -0400

    Added permissions for editing tickets

--- app/policies/ticket_policy.rb
+++ app/policies/ticket_policy.rb
@@ -1,17 +1,23 @@
 class TicketPolicy < ApplicationPolicy
-  class Scope < Scope
-    def resolve
-      scope
+    class Scope < Scope
+        def resolve
+            scope
+        end
     end
-  end
 
-  def show?
-      user.try(:admin?) || record.project.has_member?(user)
-  end
+    def show?
+        user.try(:admin?) || record.project.has_member?(user)
+    end
 
-  def create?
+    def create?
+        user.try(:admin?) ||
+            record.project.has_manager?(user) ||
+            record.project.has_editor?(user)
+    end
+
+    def update?
       user.try(:admin?) ||
-          record.project.has_manager?(user) ||
-          record.project.has_editor?(user)
-  end
+        record.project.has_manager?(user) ||
+        (record.project.has_editor?(user) && (record.author == user))
+    end
 end
