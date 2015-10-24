3455cbe01634776d9b88a7cbef60030225739488
Ernald Nicolas <enicolas@vistaprint.com>
2015-10-24 18:25:31 -0400

    Uploaded assets are now served through the AssetsController for extra security

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
      resources :tickets
   end
 
+  resources :assets, only: [:show], path: :files
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
